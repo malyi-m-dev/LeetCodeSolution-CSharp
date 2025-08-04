670344b9f85ae48c55ac257fa31697f008d50021
m.malyi <m.malyi@urent.ru>
2025-08-04 13:23:57 +0500

    add TwoSum

---
+++ LeetCodeSolution-CSharp/Easy/TwoSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeSolution_CSharp.Easy;
+
+public class TwoSum
+{
+    public int[] Main(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+
+            if (!map.ContainsKey(nums[i]))
+            {
+                map[nums[i]] = i;
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+}
---
+++ LeetCodeSolution-CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using LeetCodeSolution_CSharp.Easy;
+
+class Program
+{
+    static void Main()
+    {
+        var solutions = new Solutions();
+        solutions.GetTwoSumSolution();
+    }
+}
+
+class Solutions
+{
+    internal void GetTwoSumSolution()
+    {
+        var solver = new TwoSum();
+        int[] result = solver.Main([2, 7, 11, 15], 9);
+        Console.WriteLine($"Result: [{string.Join(", ", result)}]");
+    }
+}
